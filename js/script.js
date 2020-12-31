7c8915c994c48c9a233b3b225fa81e507e6e7ee2
Ayrus16 <suryap.sp83@gmail.com>
2020-12-31 18:21:37 +0700

    update about

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-/* -------------------------------- Navscroll ------------------------------- */
 var myNav = document.getElementById('mynav');
+var psAbout = document.getElementById('poster-about');
+var psProduct = document.getElementById('poster-product');
+
 window.onscroll = function () { 
     
     if (document.body.scrollTop >= 100 || document.documentElement.scrollTop >= 100 ) {
@@ -12,4 +14,23 @@ window.onscroll = function () {
         myNav.classList.remove("navbar-colored");
         document.getElementById('nav-brand').src='assets/img/logo-white.png';
     }
+
+    // about
+    if (document.body.scrollTop <= 400 || document.documentElement.scrollTop <= 400 ) {
+        psAbout.classList.remove("back-element");
+    } 
+    if (document.body.scrollTop >= 401 || document.documentElement.scrollTop >= 401 ) {
+        psAbout.classList.add("back-element");
+    } 
+
+    // Product
+    if (document.body.scrollTop <= 1500 || document.documentElement.scrollTop <= 1500 ) {
+        psProduct.classList.remove("back-element");
+    } 
+    if (document.body.scrollTop >= 1501 || document.documentElement.scrollTop >= 1501 ) {
+        psProduct.classList.add("back-element");
+    } 
+    
+
 };
+
